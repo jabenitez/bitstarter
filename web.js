2313cc24cdd9b40a4d7f2a476907ded604d70628
J. A. Benitez <jabenite@gmail.com>
2013-07-10 16:23:53 +0000

    added index.hmtl to the web.js

--- web.js
+++ web.js
@@ -3,7 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    try {
+	var data = new Buffer(fs.readFileSync('index.html'));
+    } catch (err) {
+	if (err.code != 'ENOENT') throw e;
+	console.log("File not found ");
+    }
+    response.send(buf.toString(data));
 });
 
 var port = process.env.PORT || 5000;
